bda137e300d8e15a6bb4daa72ea73b0bca0e2b58
tiltedlistener <tiltedlistener@gmail.com>
2016-08-05 18:54:38 -0700

    rename

---
+++ assignments/2_Ruby_intro.rb
@@ -0,0 +1,30 @@
+###
+# Copy each section separately and run through https://repl.it/languages/ruby
+# 
+# When finished, try running the entire program through the REPL
+###
+
+########## Output
+puts "Hello World!"
+
+########## Assignment
+a = 5
+b = 6
+
+########## Basic Math
+a = 5
+b = 6
+a + b
+
+########## Math Output
+a = 5 
+b = 6
+c = a + b
+puts c
+
+########## Math output with precedence
+a = 3
+b = 5
+c = (a + b)/2 - b
+puts c
+
